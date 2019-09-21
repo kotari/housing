5323133d03f639d7f02c6d27803ffd593d145ed5
af81392 <rajesh.kotari@anthem.com>
2019-09-21 13:58:41 -0700

    following a different kernel on kaggle

---
+++ multi.py
@@ -0,0 +1,321 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+pd.set_option('display.max_columns', 90)
+
+'''Seaborn and Matplotlib Visualization'''
+import matplotlib.pyplot as plt
+import seaborn as sns
+plt.style.use('bmh')                    
+sns.set_style({'axes.grid':False}) 
+
+import plotly.offline as plot
+# from plotly.offline import iplot, init_notebook_mode
+import plotly.graph_objs as go
+# init_notebook_mode(connected = True)
+import warnings as wrn
+wrn.filterwarnings('ignore', category = DeprecationWarning) 
+wrn.filterwarnings('ignore', category = FutureWarning) 
+wrn.filterwarnings('ignore', category = UserWarning)
+
+train = pd.read_csv('train.csv')
+test = pd.read_csv('test.csv')
+
+train.head()
+test.head()
+
+train.shape
+test.shape
+
+train.columns.values
+train.dtypes.value_counts()
+
+test.dtypes.value_counts()
+
+train_int = set(train.select_dtypes(include=['int64']).columns)
+test_int = set(test.select_dtypes(include=['int64']).columns)
+
+print(train_int)
+print(test_int)
+
+print(train_int-test_int)
+
+merged = pd.concat([train, test], axis=0, sort=True)
+display(merged.dtypes.value_counts())
+print('Dimensions of data:', merged.shape)
+
+num_merged = merged.select_dtypes(include = ['int64', 'float64'])
+display(num_merged.head(3))
+print('\n')
+display(num_merged.columns.values)
+
+def draw_histograms(df, variables, n_rows, n_cols):
+    fig=plt.figure()
+    for i, var_name in enumerate(variables):
+        ax=fig.add_subplot(n_rows,n_cols,i+1)
+        df[var_name].hist(bins=40,ax=ax,color = 'green',alpha=0.5, figsize = (20, 100))
+        ax.set_title(var_name, fontsize = 43)
+        ax.tick_params(axis = 'both', which = 'major', labelsize = 15)
+        ax.tick_params(axis = 'both', which = 'minor', labelsize = 15)
+        ax.set_xlabel('')
+    fig.tight_layout(rect = [0, 0.03, 1, 0.95])  # Improves appearance a bit.
+    plt.show()
+    
+draw_histograms(num_merged, num_merged.columns, 19, 2)
+
+merged.loc[:,['MSSubClass', 'OverallQual', 'OverallCond', 'MoSold', 'YrSold']] = merged.loc[:,['MSSubClass', 'OverallQual', 'OverallCond', 'MoSold', 'YrSold']].astype('object')
+
+merged.dtypes.value_counts()
+
+corr = train.corr()
+f, ax = plt.subplots(figsize=(15, 12))
+sns.heatmap(corr, linewidths=.5, vmin=0, vmax=1, square=True)
+
+k = 10 #number of variables for heatmap
+cols = corr.nlargest(k, 'SalePrice')['SalePrice'].index
+cm = np.corrcoef(train[cols].values.T)
+sns.set(font_scale=1.25)
+hm = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'size': 10}, yticklabels=cols.values, xticklabels=cols.values)
+plt.show()
+
+def scatter_plot(x, y, title, xaxis, yaxis, size, c_scale):
+    trace = go.Scatter(x = x,
+                        y = y,
+                        mode = 'markers',
+                        marker = dict(color = y, size=size, showscale = True, colorscale = c_scale))
+    layout = go.Layout(hovermode = 'closest', title = title, xaxis = dict(title = xaxis), yaxis = dict(title = yaxis))
+    fig = go.Figure(data = [trace], layout = layout)
+    return plot.plot(fig)
+
+'''Function to plot bar chart'''
+def bar_plot(x, y, title, yaxis, c_scale):
+    trace = go.Bar(x = x,
+                   y = y,
+                   marker = dict(color = y, colorscale = c_scale))
+    layout = go.Layout(hovermode= 'closest', title = title, yaxis = dict(title = yaxis))
+    fig = go.Figure(data = [trace], layout = layout)
+    return plot.plot(fig)
+
+'''Function to plot histogram'''
+def histogram_plot(x, title, yaxis, color):
+    trace = go.Histogram(x = x,
+                        marker = dict(color = color))
+    layout = go.Layout(hovermode = 'closest', title = title, yaxis = dict(title = yaxis))
+    fig = go.Figure(data = [trace], layout = layout)
+    return plot.plot(fig)
+
+scatter_plot(train.GrLivArea, train.SalePrice, 'GrLivArea vs SalePrice', 'GrLivArea', 'SalePrice', 10, 'Rainbow')
+
+train.drop(train[(train.GrLivArea>4000) & (train.SalePrice < 200000)].index, inplace = True)
+train.reset_index(drop = True, inplace = True)
+
+scatter_plot(train.GrLivArea, train.SalePrice, 'GrLivArea vs SalePrice', 'GrLivArea', 'SalePrice', 10, 'Rainbow')
+
+y_train = train.SalePrice
+
+'''Drop SalePrice from train data.'''
+train.drop('SalePrice', axis = 1, inplace = True)
+
+'''Now combine train and test data frame together.'''
+df_merged = pd.concat([train, test], axis = 0)
+
+'''Dimensions of new data frame'''
+df_merged.shape
+
+df_merged.loc[:,['MSSubClass', 'OverallQual', 'OverallCond', 'MoSold', 'YrSold']] = df_merged.loc[:,['MSSubClass', 'OverallQual', 'OverallCond', 'MoSold', 'YrSold']].astype('object')
+df_merged.dtypes.value_counts()
+
+'''columns with missing observation'''
+missing_columns = df_merged.columns[df_merged.isnull().any()].values
+'''Number of columns with missing obervation'''
+total_missing_columns = np.count_nonzero(df_merged.isnull().sum())
+print('We have ' , total_missing_columns ,  'features with missing values and those features (with missing values) are: \n\n' , missing_columns)
+
+'''Impute by None where NaN means something.'''
+to_impute_by_none = df_merged.loc[:, ['PoolQC','MiscFeature','Alley', 'Fence', 'FireplaceQu', 'GarageType', 'GarageCond','GarageFinish','GarageQual','BsmtFinType2','BsmtExposure','BsmtQual','BsmtCond','BsmtFinType1','MasVnrType']]
+for i in to_impute_by_none.columns:
+    df_merged[i].fillna('None', inplace = True)
+    
+to_impute_by_mode =  df_merged.loc[:, ['Electrical', 'MSZoning','Utilities','Exterior1st','Exterior2nd','KitchenQual','Functional', 'SaleType']]
+for i in to_impute_by_mode.columns:
+    df_merged[i].fillna(df_merged[i].mode()[0], inplace = True)
+    
+to_impute_by_median = df_merged.loc[:, ['BsmtFullBath','BsmtHalfBath', 'GarageCars', 'MasVnrArea', 'GarageYrBlt', 'BsmtFinSF1', 'BsmtFinSF2', 'BsmtUnfSF', 'TotalBsmtSF', 'GarageArea']]
+for i in to_impute_by_median.columns:
+    df_merged[i].fillna(df_merged[i].median(), inplace = True)
+
+
+df = df_merged.drop(columns=['Id','LotFrontage'], axis=1)
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+df = df.apply(le.fit_transform) # data is converted.
+df.head(2)
+
+df['LotFrontage'] = df_merged['LotFrontage']
+# Move Age at index 0.
+df = df.set_index('LotFrontage').reset_index()
+df.head(2)
+
+corr = df.corr()
+f, ax = plt.subplots(figsize=(15, 12))
+sns.heatmap(corr, linewidths=.5, vmin=0, vmax=1, square=True)
+plt.show()
+
+display(corr['LotFrontage'].sort_values(ascending = False)[:5])
+k = 10 #number of variables for heatmap
+cols = corr.nlargest(k, 'LotFrontage')['LotFrontage'].index
+cm = np.corrcoef(df[cols].values.T)
+sns.set(font_scale=1.25)
+hm = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'size': 10}, yticklabels=cols.values, xticklabels=cols.values)
+plt.show()
+
+'''Impute LotFrontage with median of respective columns (i.e., BldgType)'''
+df_merged['LotFrontage'] = df_merged.groupby(['BldgType'])['LotFrontage'].transform(lambda x: x.fillna(x.median()))
+
+'''Is there any missing values left untreated??'''
+print('Missing variables left untreated: ', df_merged.columns[df_merged.isna().any()].values)
+
+print("Skewness: %f" % y_train.skew())
+print("Kurtosis: %f" % y_train.kurt())
+
+histogram_plot(y_train, 'SalePrice without Transformation', 'Abs Frequency', 'deepskyblue')
+
+y_train = np.log1p(y_train)
+title = 'SalePrice after Transformation (skewness: {:0.4f})'.format(y_train.skew())
+histogram_plot(y_train, title, 'Abs Frequency', ' darksalmon')
+
+skew_num = pd.DataFrame(data = df_merged.select_dtypes(include = ['int64', 'float64']).skew(), columns=['Skewness'])
+skew_num_sorted = skew_num.sort_values(ascending = False, by = 'Skewness')
+skew_num_sorted
+
+bar_plot(skew_num_sorted.index, skew_num_sorted.Skewness, 'Skewness in Explanetory Variables', 'Skewness', 'Blackbody')
+
+df_merged_num = df_merged.select_dtypes(include = ['int64', 'float64'])
+
+'''Make the tranformation of the explanetory variables'''
+df_merged_skewed = np.log1p(df_merged_num[df_merged_num.skew()[df_merged_num.skew() > 0.5].index])
+
+
+#Normal variables
+df_merged_normal = df_merged_num[df_merged_num.skew()[df_merged_num.skew() < 0.5].index]
+    
+#Merging
+df_merged_num_all = pd.concat([df_merged_skewed, df_merged_normal], axis = 1)
+df_merged_num.update(df_merged_num_all)
+
+'''Standarize numeric features with RobustScaler'''
+from sklearn.preprocessing import RobustScaler
+
+'''Creating scaler object.'''
+scaler = RobustScaler()
+
+'''Fit scaler object on train data.'''
+scaler.fit(df_merged_num)
+
+'''Apply scaler object to both train and test data.'''
+df_merged_num_scaled = scaler.transform(df_merged_num)
+
+'''Retrive column names'''
+df_merged_num_scaled = pd.DataFrame(data = df_merged_num_scaled, columns = df_merged_num.columns, index = df_merged_num.index)
+
+
+"""Let's extract categorical variables first and convert them into category."""
+df_merged_cat = df_merged.select_dtypes(include = ['object']).astype('category')
+
+"""let's begin the tedious process of label encoding of ordinal variable"""
+df_merged_cat.LotShape.replace(to_replace = ['IR3', 'IR2', 'IR1', 'Reg'], value = [0, 1, 2, 3], inplace = True)
+df_merged_cat.LandContour.replace(to_replace = ['Low', 'Bnk', 'HLS', 'Lvl'], value = [0, 1, 2, 3], inplace = True)
+df_merged_cat.Utilities.replace(to_replace = ['NoSeWa', 'AllPub'], value = [0, 1], inplace = True)
+df_merged_cat.LandSlope.replace(to_replace = ['Sev', 'Mod', 'Gtl'], value = [0, 1, 2], inplace = True)
+df_merged_cat.ExterQual.replace(to_replace = ['Fa', 'TA', 'Gd', 'Ex'], value = [0, 1, 2, 3], inplace = True)
+df_merged_cat.ExterCond.replace(to_replace = ['Po', 'Fa', 'TA', 'Gd', 'Ex'], value = [0, 1, 2, 3, 4], inplace = True)
+df_merged_cat.BsmtQual.replace(to_replace = ['None', 'Fa', 'TA', 'Gd', 'Ex'], value = [0, 1, 2, 3, 4], inplace = True)
+df_merged_cat.BsmtCond.replace(to_replace = ['None', 'Po', 'Fa', 'TA', 'Gd'], value = [0, 1, 2, 3, 4], inplace = True)
+df_merged_cat.BsmtExposure.replace(to_replace = ['None', 'No', 'Mn', 'Av', 'Gd'], value = [0, 1, 2, 3, 4], inplace = True)
+df_merged_cat.BsmtFinType1.replace(to_replace = ['None', 'Unf', 'LwQ', 'Rec', 'BLQ', 'ALQ', 'GLQ'], value = [0, 1, 2, 3, 4, 5, 6], inplace = True)
+df_merged_cat.BsmtFinType2.replace(to_replace = ['None', 'Unf', 'LwQ', 'Rec', 'BLQ', 'ALQ', 'GLQ'], value = [0, 1, 2, 3, 4, 5, 6], inplace = True)
+df_merged_cat.HeatingQC.replace(to_replace = ['Po', 'Fa', 'TA', 'Gd', 'Ex'], value = [0, 1, 2, 3, 4], inplace = True)
+df_merged_cat.Electrical.replace(to_replace = ['Mix', 'FuseP', 'FuseF', 'FuseA', 'SBrkr'], value = [0, 1, 2, 3, 4], inplace = True)
+df_merged_cat.KitchenQual.replace(to_replace = ['Fa', 'TA', 'Gd', 'Ex'], value = [0, 1, 2, 3], inplace = True)
+df_merged_cat.Functional.replace(to_replace = ['Sev', 'Maj2', 'Maj1', 'Mod', 'Min2', 'Min1', 'Typ'], value = [0, 1, 2, 3, 4, 5, 6], inplace = True)
+df_merged_cat.FireplaceQu.replace(to_replace =  ['None', 'Po', 'Fa', 'TA', 'Gd', 'Ex'], value = [0, 1, 2, 3, 4, 5], inplace = True)
+df_merged_cat.GarageFinish.replace(to_replace =  ['None', 'Unf', 'RFn', 'Fin'], value = [0, 1, 2, 3], inplace = True)
+df_merged_cat.GarageQual.replace(to_replace =  ['None', 'Po', 'Fa', 'TA', 'Gd', 'Ex'], value = [0, 1, 2, 3, 4, 5], inplace = True)
+df_merged_cat.GarageCond.replace(to_replace =  ['None', 'Po', 'Fa', 'TA', 'Gd', 'Ex'], value = [0, 1, 2, 3, 4, 5], inplace = True)
+df_merged_cat.PavedDrive.replace(to_replace =  ['N', 'P', 'Y'], value = [0, 1, 2], inplace = True)
+df_merged_cat.PoolQC.replace(to_replace =  ['None', 'Fa', 'Gd', 'Ex'], value = [0, 1, 2, 3], inplace = True)
+df_merged_cat.Fence.replace(to_replace =  ['None', 'MnWw', 'GdWo', 'MnPrv', 'GdPrv'], value = [0, 1, 2, 3, 4], inplace = True)
+
+
+'''All the encodeded variables have int64 dtype except OverallQual and OverallCond. So convert them back into int64.'''
+df_merged_cat.loc[:, ['OverallQual', 'OverallCond']] = df_merged_cat.loc[:, ['OverallQual', 'OverallCond']].astype('int64')
+
+'''Extract label encoded variables'''
+df_merged_label_encoded = df_merged_cat.select_dtypes(include = ['int64'])
+
+'''Now selecting the nominal vaiables for one hot encording'''
+df_merged_one_hot = df_merged_cat.select_dtypes(include=['category'])
+
+"""Let's get the dummies variable"""
+df_merged_one_hot = pd.get_dummies(df_merged_one_hot, drop_first=True)
+
+"""Let's concat one hot encoded and label encoded variable together"""
+df_merged_encoded = pd.concat([df_merged_one_hot, df_merged_label_encoded], axis=1)
+
+'''Finally join processed categorical and numerical variables'''
+df_merged_processed = pd.concat([df_merged_num_scaled, df_merged_encoded], axis=1)
+
+'''Dimensions of new data frame'''
+df_merged_processed.shape
+
+'''Now retrive train and test data for modelling.'''
+df_train_final = df_merged_processed.iloc[0:1458, :]
+df_test_final = df_merged_processed.iloc[1458:, :]
+
+'''And we have our target variable as y_train.'''
+y_train = y_train
+
+
+'''Create a function to tune hyperparameters of the selected models.'''
+def tune_hyperparameters(model, param_grid):
+    from sklearn.model_selection import GridSearchCV
+    global best_params, best_score #if you want to know best parametes and best score
+    
+    '''Construct grid search object with 10 fold cross validation.'''
+    grid = GridSearchCV(model, param_grid, cv = 10, verbose = 1, scoring = 'neg_mean_squared_error', n_jobs = -1)
+    grid.fit(df_train_final, y_train)
+    best_params = grid.best_params_ 
+    best_score = np.round(np.sqrt(-1 * grid.best_score_), 5)
+    return best_params, best_score
+
+from xgboost import XGBRegressor
+xgb_params_grid = {'min_child_weight': [5, 10],
+                   'gamma': [0.04, 0.1, 1.5],
+                   'subsample': [0.6, 0.8, 1.0],
+                   'colsample_bytree': [0.46, 1.0],
+                   'max_depth': [3, 4]}
+xgb_opt = XGBRegressor(learning_rate = 0.03, reg_alpha = 0.4640, reg_lambda = 0.8571, n_estimators = 1000, 
+                       silent = 1, nthread = -1, random_state = 101)
+
+tune_hyperparameters(xgb_opt, xgb_params_grid)
+xgb_best_params, xgb_best_score = best_params, best_score
+print('XGB best params:{} & best_score:{:0.5f}' .format(xgb_best_params, xgb_best_score))
+
+# XGB best params:{'colsample_bytree': 0.46, 'gamma': 0.04, 'max_depth': 3, 'min_child_weight': 5, 'subsample': 0.6} & best_score:0.11501
+
+xgbr = XGBRegressor(learning_rate = 0.03, reg_alpha = 0.4640, reg_lambda = 0.8571, n_estimators = 1000, 
+                       colsample_bytree= 0.46, gamma= 0.04, max_depth= 3, min_child_weight= 5, silent = 1,
+                       subsample= 0.6, nthread = -1, random_state = 101)
+
+xgbr.fit(df_train_final, y_train)
+predictions = xgbr.predict(df_test_final)
+predictions = np.expm1(predictions)
+
+
+output = pd.DataFrame(test['Id'])
+output['SalePrice'] = predictions
+
+output.to_csv('predictions.csv', index=False)
+
+
+# https://www.kaggle.com/vikassingh1996/extensive-data-preprocessing-and-modeling/notebook
